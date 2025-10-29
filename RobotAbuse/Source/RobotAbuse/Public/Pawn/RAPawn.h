aa1d0f9e8ee75743eb90451dce2ea9cc1851720b
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2025-10-29 11:33:53 -0700

    Added C++ files to project. Begin migration over of BPs from prototype to C++ for reuse/clarity/debugging. Added RAPawn derived from DefaultPawn and set parent of BP_RAPawn ro RAPawn.

---
+++ RobotAbuse/Source/RobotAbuse/Public/Pawn/RAPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DefaultPawn.h"
+#include "RAPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTABUSE_API ARAPawn : public ADefaultPawn
+{
+	GENERATED_BODY()
+	
+};
